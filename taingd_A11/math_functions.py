6ac40b43c5a56aac82bd477b23109b59aa926a6c
Devin Taing <96029306+devintaing@users.noreply.github.com>
2024-11-17 17:58:41 -0800

    Add files via upload

---
+++ taingd_A11/math_functions.py
@@ -0,0 +1,8 @@
+#math_functions.py
+def power(a, b):
+  return a ** b
+
+def divide(a, b):
+  if b == 0:
+    return "You cannot divide by 0."
+  return a / b
---
+++ taingd_A11/unitTest_math_functions.py
@@ -0,0 +1,24 @@
+import unittest
+from math_functions import power, divide
+
+class TestMathFunctions(unittest.TestCase):
+  def test_divide(self):
+    self.assertEqual(divide(10, 4), 2.5)
+    self.assertEqual(divide(10, 2), 5)
+    self.assertEqual(divide(10, 0), "You cannot divide by 0.")
+    self.assertEqual(divide(0, 2), 0)
+    self.assertEqual(divide(-10, 2), -5)
+    self.assertEqual(divide(10, -2), -5)
+    self.assertEqual(divide(-10, -2), 5)
+
+  def test_power(self):
+    self.assertEqual(power(2, 3), 8)
+    self.assertEqual(power(5, 0), 1)
+    self.assertEqual(power(4, -3), 0.015625)
+    self.assertEqual(power(-4, 3), -64)
+    self.assertEqual(power(0, 3), 0)
+    self.assertEqual(power(2, 0.5), 1.4142135623730951)
+    self.assertEqual(power(0.5, 2), 0.25)
+
+if __name__ == '__main__':
+  unittest.main()
